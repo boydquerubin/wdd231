283699151a341fb4c8e12e684de73f32239a7421
Boyd Querubin <boydisaacq@gmail.com>
2025-03-13 13:21:41 -0600

    prophet

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,25 +1,41 @@
 const cards = document.querySelector('#cards');
-// async function getProphetData() {
-//   const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json"
-//   try {
-//     const response = await fetch(url)
-//     if (!response.ok) {
-//       throw new Error(`Response status: ${response.status}`);
-//     }
-
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
-
-// getProphetData()
+const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json"
 
 async function getProphetData() {
   const response = await fetch(url);
   const data = await response.json();
-  console.table(data.prophets); // temporary testing of data response
+  // console.table(data.prophets);
+  displayProphets(data.prophets);
 }
 
 getProphetData();
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+
+    let card = document.createElement('section');
+    let fullName = document.createElement('h2'); // fill in the blank
+    let portrait = document.createElement('img');
+    let dob = document.createElement('p');
+    let pob = document.createElement('p');
+
+    // Build the h2 content out to show the prophet's full name
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+    dob.textContent = `Date of birth: ${prophet.birthdate}`
+    pob.textContent = `Place of birth: ${prophet.birthplace}`
+    // Build the image portrait by setting all the relevant attributes
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    // Append the section(card) with the created elements
+    card.appendChild(fullName); //fill in the blank
+    card.appendChild(portrait);
+    card.appendChild(dob);
+    card.appendChild(pob);
+
+    cards.appendChild(card);
+  })
+}
