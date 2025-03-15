25ffd58b4a50f00b6ff7ab8e18d40108cb6f28ae
Boyd Querubin <boydisaacq@gmail.com>
2025-03-15 10:14:21 -0600

    checkpoint

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,17 +1,32 @@
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector("#menu");
 
-hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
+hambutton.addEventListener("click", () => {
+  mainnav.classList.toggle("show");
+  hambutton.classList.toggle("show");
+});
+
+const display = document.querySelector("article");
+const menu = document.querySelector(".menu");
+
+
+display.classList.add("grid");
+
+menu.addEventListener("click", (event) => {
+  if (event.target.id === "grid") {
+    display.classList.add("grid");
+    display.classList.remove("list");
+  } else if (event.target.id === "list") {
+    display.classList.add("list");
+    display.classList.remove("grid");
+  }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   const currentPage = window.location.pathname.split("/").pop();
-  const navLinks = document.querySelectorAll(".navigation a");
-
-  navLinks.forEach(link => {
-    if (link.getAttribute("href") === currentPage || (currentPage === "directory.html" && link.getAttribute("href") === "#")) {
+  document.querySelectorAll(".navigation a").forEach(link => {
+    if (link.getAttribute("href") === currentPage || 
+        (currentPage === "directory.html" && link.getAttribute("href") === "#")) {
       link.classList.add("active");
     }
   });
