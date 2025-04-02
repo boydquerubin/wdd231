babc1cf1cdcf4a71a18423b3ce7adfed8779536c
Boyd Querubin <boydisaacq@gmail.com>
2025-04-02 01:26:08 -0600

    siteplan

---
+++ project/scripts/navigation.js
@@ -0,0 +1,18 @@
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const currentPage = window.location.pathname.split("/").pop();
+  const navLinks = document.querySelectorAll(".navigation a");
+
+  navLinks.forEach(link => {
+    if (link.getAttribute("href") === currentPage || (currentPage === "index.html" && link.getAttribute("href") === "#")) {
+      link.classList.add("active");
+    }
+  });
+});
